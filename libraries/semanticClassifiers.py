18f4f43c8dc2b0e8bc42104a2f741456997745ed
Bogdan Petre <bogpetre@gmail.com>
2020-10-24 18:42:56 -0400

    docTopTransformer2

--- libraries/semanticClassifiers.py
+++ libraries/semanticClassifiers.py
@@ -6,6 +6,7 @@ import numpy as np
 from gensim.models import LsiModel as lsi
 from gensim.corpora import Dictionary
 from gensim import matutils
+from gensim.models.phrases import Phrases, Phraser
 
 from sklearn.base import TransformerMixin, BaseEstimator
 
@@ -46,3 +47,58 @@ class docTopTransformer(TransformerMixin, BaseEstimator):
         X_ = topics_csr.T.toarray()
         
         return X_
+    
+        
+# similar to docTopTransformer except it takes a corpus as input and trains dictionaries and computes BOWs internally
+class docTopTransformer2(TransformerMixin, BaseEstimator):
+    
+    def __init__(self, d=300, distributed=False):
+        self.this_dict = []
+        self.d = d
+        self.distributed = distributed
+        
+    def _getBOW(self,X):
+        # transform corpus (train) into a 2d array word counts (a 'bag of words')
+        bow = [self.this_dict.doc2bow(text) for text in X]
+        
+        return bow
+    
+    # takes corpus as input
+    def fit(self, X, y=None):
+        
+        # train a document-topic model        
+        self.this_dict = Dictionary(X)
+
+        bow = self._getBOW(X)
+        
+        # construct a semantic model based on document-topic similarity (15-20 min for 1500k reviews?)
+        self.semSpace = lsi(bow, id2word=self.this_dict, num_topics=self.d, 
+                            chunksize=10000, distributed=self.distributed)
+        
+        return self
+    
+    def transform(self, X, y=None):
+        bow = self._getBOW(X)
+        
+        # Apply the semantic model to the training set bag of words (fast)
+        feat = self.semSpace[bow]
+
+        # convert from TransformedCorpus datatype to numpy doc x topic array (medium speed, needs more benchmarking)
+        topics_csr = matutils.corpus2csc(feat)
+        X_ = topics_csr.T.toarray()
+        
+        return X_
+    
+class bigramsPhraser(TransformerMixin, BaseEstimator):
+    
+    def fit(self, X, y=None):
+        phrases = Phrases(X, min_count=1, threshold=1);
+        self.phraser = Phraser(phrases)
+        
+        return self
+    
+    def transform(self, X, y=None):
+        return [self.phraser[x] for x in X]
+
+        
+    
